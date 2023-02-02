960c518800124485312922dbb22cb62789edfea5
Emanuel-san <emanuel.sanchez@ha.ax>
2023-02-02 17:36:08 +0200

    added thymleaf maven dependency

--- src/main/java/app/mortageweb/controllers/MortageController.java
+++ src/main/java/app/mortageweb/controllers/MortageController.java
@@ -2,12 +2,14 @@ package app.mortageweb.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class MortageController {
 
-    @GetMapping
+    @GetMapping("/")
     public String index(){
+        //ModelAndView
         return "response";
     }
 }
